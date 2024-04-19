72a77f10d66baa4f23cd890990fa463ab00bae49
pancake <pancake@nopcode.org>
2024-04-19 18:59:35 +0200

    Pack all globals into a single struct

--- src/core_r2yara.c
+++ src/core_r2yara.c
@@ -16,27 +16,18 @@ static inline char *r_str_after(char *s, char c) {
 }
 #endif
 
-// TODO: remove globals!
-static R_TH_LOCAL bool initialized = false;
-static R_TH_LOCAL bool print_strings = false;
-static R_TH_LOCAL unsigned int flagidx = 0;
-static R_TH_LOCAL bool io_va = true;
-static R_TH_LOCAL RList* rules_list = NULL;
+typedef struct {
+	bool initialized;
+	bool print_strings;
+	unsigned int flagidx;
+	bool iova; // true
+	RList* rules_list;
+	RCore *core;
+} R2Yara;
 
-#if YR_MAJOR_VERSION < 4
-static int callback(int message, void* rule, void* data);
-#else
-static int callback(YR_SCAN_CONTEXT* context, int message, void* rule, void* data);
-#endif
-static int cmd_yara_add(const RCore* core, const char* input);
-static int cmd_yara_add_file(const char* rules_path);
-static int cmd_yara_call(void *user, const char *input);
-static int cmd_yara_clear();
-static int cmd_yara_init(void *user, const char *cmd);
-static int cmd_yara_process(const RCore* core, const char* input);
-static int cmd_yara_scan(const RCore* core, const char* option);
-static int cmd_yara_load_default_rules(const RCore* core);
-static int cmd_yara_version();
+// RCorePlugins have no session yet // R2_600
+// TODO: remove globals when r2-6.0.0 ships RCoreSession
+static R_TH_LOCAL R2Yara Gr2yara = {0};
 
 static const char yara_rule_template[] = "rule RULE_NAME {\n  strings:\n\n  condition:\n}";
 
@@ -46,8 +37,9 @@ static const char yara_rule_template[] = "rule RULE_NAME {\n  strings:\n\n  cond
  */
 
 #if YR_MAJOR_VERSION < 4
-static int callback (int message, void *msg_data, void *user_data) {
-	RCore *core = (RCore *) user_data;
+static int callback(int message, void *msg_data, void *user_data) {
+	R2Yara *r2yara = (R2Yara *)user_data;
+	RCore *core = r2yara->core;
 	RPrint *print = core->print;
 	unsigned int ruleidx;
 	st64 offset = 0;
@@ -65,15 +57,14 @@ static int callback (int message, void *msg_data, void *user_data) {
 			yr_string_matches_foreach (string, match) {
 				n = match->base + match->offset;
 				// Find virtual address if needed
-				if (io_va) {
+				if (r2yara->iova) {
 					RIOMap *map = r_io_map_get_paddr (core->io, n);
 					if (map) {
 						offset = r_io_map_begin (map) - map->delta;
 					}
 				}
-
 				r_strf_var (flag, 256, "%s%d.%s_%d", "yara", flagidx, rule->identifier, ruleidx);
-				if (print_strings) {
+				if (r2yara->print_strings) {
 					r_cons_printf ("0x%08" PFMT64x ": %s : ", n + offset, flag);
 					r_print_bytes (print, match->data, match->data_length, "%02x");
 				}
@@ -94,9 +85,9 @@ static void compiler_callback(int error_level, const char* file_name,
 }
 #else
 static int callback(YR_SCAN_CONTEXT* context, int message, void *msg_data, void *user_data) {
-	RCore *core = (RCore *) user_data;
+	R2Yara *r2yara = (R2Yara *)user_data;
+	RCore *core = r2yara->core;
 	RPrint *print = core->print;
-	unsigned int ruleidx;
 	st64 offset = 0;
 	ut64 n = 0;
 
@@ -105,21 +96,20 @@ static int callback(YR_SCAN_CONTEXT* context, int message, void *msg_data, void 
 	if (message == CALLBACK_MSG_RULE_MATCHING) {
 		YR_STRING* string;
 		r_cons_printf ("%s\n", rule->identifier);
-		ruleidx = 0;
+		unsigned int ruleidx = 0;
 		yr_rule_strings_foreach (rule, string) {
 			YR_MATCH* match;
 			yr_string_matches_foreach (context, string, match) {
 				n = match->base + match->offset;
 				// Find virtual address if needed
-				if (io_va) {
+				if (r2yara->iova) {
 					RIOMap *map = r_io_map_get_paddr (core->io, n);
 					if (map) {
 						offset = r_io_map_begin (map) - map->delta;
 					}
 				}
-
-				r_strf_var (flag, 256, "%s%d.%s_%d", "yara", flagidx, rule->identifier, ruleidx);
-				if (print_strings) {
+				r_strf_var (flag, 256, "yara%d.%s_%d", r2yara->flagidx, rule->identifier, ruleidx);
+				if (r2yara->print_strings) {
 					r_cons_printf ("0x%08" PFMT64x ": %s : ", n + offset, flag);
 					r_print_bytes (print, match->data, match->data_length, "%02x");
 				}
@@ -127,7 +117,7 @@ static int callback(YR_SCAN_CONTEXT* context, int message, void *msg_data, void 
 				ruleidx++;
 			}
 		}
-		flagidx++;
+		r2yara->flagidx++;
 
 	}
 	return CALLBACK_CONTINUE;
@@ -141,23 +131,24 @@ static void compiler_callback(int error_level, const char* file_name,
 }
 #endif
 
-static int cmd_yara_scan(const RCore* core, R_NULLABLE const char* option) {
+static int cmd_yara_scan(R2Yara *r2yara, R_NULLABLE const char* option) {
+	RCore *core = r2yara->core;
 	RListIter* rules_it;
 	YR_RULES* rules;
 
 	r_flag_space_push (core->flags, "yara");
-	const unsigned int to_scan_size = r_io_size (core->io);
-	io_va = r_config_get_b (core->config, "io.va");
+	const size_t to_scan_size = r_io_size (core->io);
+	r2yara->iova = r_config_get_b (core->config, "io.va");
 
 	if (to_scan_size < 1) {
 		R_LOG_ERROR ("Invalid file size");
 		return false;
 	}
 
-	print_strings = true;
+	r2yara->print_strings = true;
 	if (option != NULL) {
 		if (*option == 'q') {
-			print_strings = false;
+			r2yara->print_strings = false;
 		} else {
 			R_LOG_ERROR ("Invalid option");
 			return false;
@@ -176,21 +167,21 @@ static int cmd_yara_scan(const RCore* core, R_NULLABLE const char* option) {
 		free (to_scan);
 		return false;
 	}
-	r_list_foreach (rules_list, rules_it, rules) {
-		yr_rules_scan_mem (rules, to_scan, to_scan_size, 0, callback, (void *)core, 0);
+	r_list_foreach (r2yara->rules_list, rules_it, rules) {
+		yr_rules_scan_mem (rules, to_scan, to_scan_size, 0, callback, (void*)&r2yara, 0);
 	}
 	free (to_scan);
 
 	return true;
 }
 
-static int cmd_yara_show(const char * name) {
+static int cmd_yara_show(R2Yara *r2yara, const char * name) {
 	/* List loaded rules containing name */
 	RListIter* rules_it;
 	YR_RULES* rules;
 	YR_RULE* rule;
 
-	r_list_foreach (rules_list, rules_it, rules) {
+	r_list_foreach (r2yara->rules_list, rules_it, rules) {
 		yr_rules_foreach (rules, rule) {
 			if (r_str_casestr (rule->identifier, name)) {
 				r_cons_printf ("%s\n", rule->identifier);
@@ -201,7 +192,7 @@ static int cmd_yara_show(const char * name) {
 	return true;
 }
 
-static int cmd_yara_tags() {
+static int cmd_yara_tags(R2Yara *r2yara) {
 	/* List tags from all the different loaded rules */
 	RListIter* rules_it;
 	RListIter *tags_it;
@@ -211,7 +202,7 @@ static int cmd_yara_tags() {
 	RList *tag_list = r_list_new();
 	tag_list->free = free;
 
-	r_list_foreach (rules_list, rules_it, rules) {
+	r_list_foreach (r2yara->rules_list, rules_it, rules) {
 		yr_rules_foreach(rules, rule) {
 			yr_rule_tags_foreach(rule, tag_name) {
 				if (! r_list_find (tag_list, tag_name, (RListComparator)strcmp)) {
@@ -232,14 +223,14 @@ static int cmd_yara_tags() {
 	return true;
 }
 
-static int cmd_yara_tag(const char * search_tag) {
+static int cmd_yara_tag(R2Yara *r2yara, const char * search_tag) {
 	/* List rules with tag search_tag */
 	RListIter* rules_it;
 	YR_RULES* rules;
 	YR_RULE* rule;
 	const char* tag_name;
 
-	r_list_foreach (rules_list, rules_it, rules) {
+	r_list_foreach (r2yara->rules_list, rules_it, rules) {
 		yr_rules_foreach (rules, rule) {
 			yr_rule_tags_foreach(rule, tag_name) {
 				R_LOG_WARN ("Invalid option");
@@ -254,26 +245,26 @@ static int cmd_yara_tag(const char * search_tag) {
 	return true;
 }
 
-static int cmd_yara_list() {
+static int cmd_yara_list(R2Yara *r2yara) {
 	/* List all loaded rules */
 	RListIter* rules_it;
 	YR_RULES* rules;
 	YR_RULE* rule;
 
-	r_list_foreach (rules_list, rules_it, rules) {
+	r_list_foreach (r2yara->rules_list, rules_it, rules) {
 		yr_rules_foreach (rules, rule) {
 			r_cons_printf ("%s\n", rule->identifier);
 		}
 	}
-	return true;
+	return 0;
 }
 
-static int cmd_yara_clear() {
+static int cmd_yara_clear(R2Yara *r2yara) {
 	/* Clears all loaded rules */
-	r_list_free (rules_list);
-	rules_list = r_list_newf ((RListFree) yr_rules_destroy);
+	r_list_free (r2yara->rules_list);
+	r2yara->rules_list = r_list_newf ((RListFree) yr_rules_destroy);
 	R_LOG_INFO ("Rules cleared");
-	return true;
+	return 0;
 }
 
 static void logerr(YR_COMPILER* compiler, R_NULLABLE const char *arg) {
@@ -286,7 +277,55 @@ static void logerr(YR_COMPILER* compiler, R_NULLABLE const char *arg) {
 	}
 }
 
-static int cmd_yara_add(const RCore* core, const char* input) {
+static int cmd_yara_add_file(R2Yara *r2yara, const char* rules_path) {
+	YR_COMPILER* compiler = NULL;
+	YR_RULES* rules;
+
+	if (!rules_path) {
+		R_LOG_INFO ("Please tell me what am I supposed to load");
+		return false;
+	}
+
+	FILE* rules_file = r_sandbox_fopen (rules_path, "r");
+	if (!rules_file) {
+		R_LOG_ERROR ("Unable to open %s", rules_path);
+		return false;
+	}
+
+	if (yr_compiler_create (&compiler) != ERROR_SUCCESS) {
+		logerr (compiler, NULL);
+		goto err_exit;
+	}
+
+	int result = yr_compiler_add_file (compiler, rules_file, NULL, rules_path);
+	fclose (rules_file);
+	rules_file = NULL;
+	if (result > 0) {
+		logerr (compiler, rules_path);
+		goto err_exit;
+	}
+
+	if (yr_compiler_get_rules (compiler, &rules) != ERROR_SUCCESS) {
+		logerr (compiler, NULL);
+		goto err_exit;
+	}
+
+	r_list_append (r2yara->rules_list, rules);
+
+	yr_compiler_destroy (compiler);
+	return true;
+
+err_exit:
+	if (compiler) {
+		yr_compiler_destroy (compiler);
+	}
+	if (rules_file) {
+		fclose (rules_file);
+	}
+	return false;
+}
+
+static int cmd_yara_add(R2Yara *r2yara, const char* input) {
 	if (!input) {
 		R_LOG_ERROR ("Missing argument");
 		return false;
@@ -297,7 +336,7 @@ static int cmd_yara_add(const RCore* core, const char* input) {
 
 	for (i = 0; input[i]; i++) {
 		if (input[i] != ' ') {
-			return cmd_yara_add_file (input + i);
+			return cmd_yara_add_file (r2yara, input + i);
 		}
 	}
 
@@ -309,7 +348,7 @@ static int cmd_yara_add(const RCore* core, const char* input) {
 	char *old_template = strdup (yara_rule_template);
 	char *modified_template = NULL;
 	do {
-		char *modified_template = r_core_editor (core, NULL, old_template);
+		char *modified_template = r_core_editor (r2yara->core, NULL, old_template);
 		R_FREE (old_template);
 		if (!modified_template) {
 			R_LOG_ERROR ("Something bad happened with the temp file");
@@ -344,55 +383,7 @@ err_exit:
 	return false;
 }
 
-static int cmd_yara_add_file(const char* rules_path) {
-	YR_COMPILER* compiler = NULL;
-	YR_RULES* rules;
-
-	if (!rules_path) {
-		R_LOG_INFO ("Please tell me what am I supposed to load");
-		return false;
-	}
-
-	FILE* rules_file = r_sandbox_fopen (rules_path, "r");
-	if (!rules_file) {
-		R_LOG_ERROR ("Unable to open %s", rules_path);
-		return false;
-	}
-
-	if (yr_compiler_create (&compiler) != ERROR_SUCCESS) {
-		logerr (compiler, NULL);
-		goto err_exit;
-	}
-
-	int result = yr_compiler_add_file (compiler, rules_file, NULL, rules_path);
-	fclose (rules_file);
-	rules_file = NULL;
-	if (result > 0) {
-		logerr (compiler, rules_path);
-		goto err_exit;
-	}
-
-	if (yr_compiler_get_rules (compiler, &rules) != ERROR_SUCCESS) {
-		logerr (compiler, NULL);
-		goto err_exit;
-	}
-
-	r_list_append (rules_list, rules);
-
-	yr_compiler_destroy (compiler);
-	return true;
-
-err_exit:
-	if (compiler) {
-		yr_compiler_destroy (compiler);
-	}
-	if (rules_file) {
-		fclose (rules_file);
-	}
-	return false;
-}
-
-static int cmd_yara_version() {
+static int cmd_yara_version(R2Yara *r2yara) {
 	r_cons_printf ("r2 %s\n", R2_VERSION);
 	r_cons_printf ("yara %s\n", YR_VERSION);
 	r_cons_printf ("r2yara %s\n", R2Y_VERSION);
@@ -426,7 +417,7 @@ const char *long_help_message[] = {
 	NULL
 };
 
-static int cmd_yara_process(const RCore* core, const char* input) {
+static int cmd_yara_process(R2Yara *r2yara, const char* input) {
 	char *inp = strdup (input);
 	char *arg = r_str_after (inp, ' ');
 	if (arg) {
@@ -434,31 +425,31 @@ static int cmd_yara_process(const RCore* core, const char* input) {
 	}
 	int res = -1;
 	if (r_str_startswith (input, "add")) {
-		res = cmd_yara_add (core, arg);
+		res = cmd_yara_add (r2yara, arg);
 	} else if (r_str_startswith (inp, "clear")) {
-		res = cmd_yara_clear ();
+		res = cmd_yara_clear (r2yara);
 	} else if (r_str_startswith (inp, "list")) {
-		res = cmd_yara_list ();
+		res = cmd_yara_list (r2yara);
 	} else if (r_str_startswith (inp, "scanS") || r_str_startswith (inp, "scan S")) {
-		res = cmd_yara_scan (core, "q");
+		res = cmd_yara_scan (r2yara, "q");
 	} else if (r_str_startswith (inp, "scan")) {
-		res = cmd_yara_scan (core, arg);
+		res = cmd_yara_scan (r2yara, arg);
 	} else if (r_str_startswith (inp, "show")) {
-		res = cmd_yara_show (arg);
+		res = cmd_yara_show (r2yara, arg);
 	} else if (r_str_startswith (inp, "tags")) {
-		res = cmd_yara_tags ();
+		res = cmd_yara_tags (r2yara);
 	} else if (r_str_startswith (input, "tag ")) {
-		res = cmd_yara_tag (arg);
+		res = cmd_yara_tag (r2yara, arg);
 	} else if (r_str_startswith (input, "ver")) {
-		res = cmd_yara_version ();
+		res = cmd_yara_version (r2yara);
 	} else {
-		r_core_cmd_help (core, long_help_message);
+		r_core_cmd_help (r2yara->core, long_help_message);
 	}
 	free (inp);
 	return res;
 }
 
-static int cmd_yr(RCore *core, const char *input) {
+static int cmd_yr(R2Yara *r2yara, const char *input) {
 	char *inp = strdup (input);
 	char *arg = r_str_after (inp, ' ');
 	int res = -1;
@@ -467,59 +458,43 @@ static int cmd_yr(RCore *core, const char *input) {
 	}
 	switch (*input) {
 	case '?': // "yr?"
-		r_core_cmd_help (core, short_help_message);
+		r_core_cmd_help (r2yara->core, short_help_message);
 		break;
 	case '/': // "yr/" <- imho makes more sense
 	case 's': // "yrs"
 		if (input[1] == 'q') {
-			res = cmd_yara_scan (core, "q");
+			res = cmd_yara_scan (r2yara, "q");
 		} else {
-			res = cmd_yara_scan (core, arg);
+			res = cmd_yara_scan (r2yara, arg);
 		}
 		break;
 	case '+':
 	case ' ':
-		cmd_yara_add (core, arg);
+		cmd_yara_add (r2yara, arg);
 		break;
 	case '-':
-		cmd_yara_clear ();
+		cmd_yara_clear (r2yara);
 		break;
 	case 't': // "yrs"
 		if (input[1]) {
-			cmd_yara_tag (arg);
+			cmd_yara_tag (r2yara, arg);
 		} else {
-			cmd_yara_tags ();
+			cmd_yara_tags (r2yara);
 		}
 		break;
 	case 'v': // "yrv"
-		res = cmd_yara_version ();
+		res = cmd_yara_version (r2yara);
 		break;
 	case 0:
-		cmd_yara_list ();
+		cmd_yara_list (r2yara);
 		break;
 	}
 	free (inp);
 	return res;
 }
 
-static int cmd_yara_call(void *user, const char *input) {
-	RCore* core = (RCore*) user;
-	if (r_str_startswith (input, "yr")) {
-		cmd_yr (core, input + 2);
-		return true;
-	}
-	if (!r_str_startswith (input, "yara")) {
-		return false;
-	}
-	if (!initialized && !cmd_yara_init (core, NULL)) {
-		return false;
-	}
-	const char *args = input[4]? input + 5: input + 4;
-	cmd_yara_process (core, args);
-	return true;
-}
-
-static int cmd_yara_load_default_rules(const RCore* core) {
+static int cmd_yara_load_default_rules(R2Yara *r2yara) {
+	RCore* core = r2yara->core;
 	RListIter* iter = NULL;
 	YR_COMPILER* compiler = NULL;
 	YR_RULES* yr_rules = NULL;
@@ -567,7 +542,7 @@ static int cmd_yara_load_default_rules(const RCore* core) {
 		goto err_exit;
 	}
 
-	r_list_append (rules_list, yr_rules);
+	r_list_append (r2yara->rules_list, yr_rules);
 
 	if (compiler) {
 		yr_compiler_destroy (compiler);
@@ -586,19 +561,46 @@ err_exit:
 
 static int cmd_yara_init(void *user, const char *cmd) {
 	RCore* core = (RCore *)user;
-	rules_list = r_list_newf ((RListFree) yr_rules_destroy);
+
+	R2Yara *r2yara = R_NEW0 (R2Yara);
+	r2yara->iova = true;
+	r2yara->core = core;
+	r2yara->rules_list = r_list_newf ((RListFree) yr_rules_destroy);
+
 	yr_initialize ();
-	cmd_yara_load_default_rules (core);
-	initialized = true;
-	flagidx = 0;
+
+	cmd_yara_load_default_rules (r2yara);
+	r2yara->initialized = true;
+	r2yara->flagidx = 0;
+	return true;
+}
+
+static int cmd_yara_call(void *user, const char *input) {
+	R2Yara *r2yara = &Gr2yara;
+	RCore* core = (void*)user; // Gr2yara->core;
+	if (r_str_startswith (input, "yr")) {
+		cmd_yr (r2yara, input + 2);
+		return true;
+	}
+	if (!r_str_startswith (input, "yara")) {
+		return false;
+	}
+	if (!r2yara->initialized) {
+		if (!cmd_yara_init (r2yara, NULL)) {
+			return false;
+		}
+	}
+	const char *args = input[4]? input + 5: input + 4;
+	cmd_yara_process (r2yara, args);
 	return true;
 }
 
 static int cmd_yara_fini(void *user, const char *cmd) {
-	if (initialized) {
-		r_list_free (rules_list);
+	R2Yara *r2yara = &Gr2yara;
+	if (r2yara->initialized) {
+		r_list_free (r2yara->rules_list);
 		yr_finalize ();
-		initialized = false;
+		r2yara->initialized = false;
 	}
 	return true;
 }
